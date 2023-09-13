a1108b92f8751d1c8e6a7a98475c9aa680ec053b
Vithya-kprcas <144312446+Vithya-kprcas@users.noreply.github.com>
2023-09-13 12:29:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+def factorial(x):
+   
+
+    if x == 1:
+        return 1
+    else:
+        
+        return (x * factorial(x-1))
+
+
+num = 7
+num = int(input("Enter a number: ")) 
+result = factorial(num)
+print("The factorial of", num, "is", result)
